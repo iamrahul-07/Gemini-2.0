70e82f32020f52c35c141ef0c749c89c9fa6b97f
Rahul <iamklrahul45@gmail.com>
2025-09-17 23:40:22 +0530

    File Upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./Main/Main"
+import Sidebar from "./Sidebar/Sidebar"
+import './Responsive/Responsive.css'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
